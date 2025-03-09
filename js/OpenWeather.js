870d51cd3f47266e315c3fe11f8a89fbeb1df7c9
Tobias-Fermiano <87425799+Tobias-Fermiano@users.noreply.github.com>
2025-03-09 15:24:03 -0300

    Ajustes LocalStorage

--- js/OpenWeather.js
+++ js/OpenWeather.js
@@ -5,17 +5,20 @@ class OpenWeather {
     this.latitude = "";
     this.longitude = "";
     this.key = "85946df34d02fb0319c8f871d4cbfa7f";
-    this.cep = "";
+    this.cep = this.recuperarCeps();
   }
 
   async buscarCep(value1, value2) {
     let lat = value1;
     let lon = value2;
+    let consultarBtn = document.getElementById("btn-buscar");
+    consultarBtn.disabled = true;
 
     if (!lat || !lon) {
       alert("Informe o CEP");
       return this.manualmente();
     } else {
+      consultarBtn.disabled = false;
       return this.getClima({
         location: { coordinates: { latitude: lat, longitude: lon } },
       });
@@ -23,14 +26,25 @@ class OpenWeather {
   }
 
   async manualmente() {
+    let consultarBtn = document.getElementById("btn-buscar");
+    consultarBtn.disabled = true;
+
     try {
       let cepInformado = document.getElementById("cep").value;
+
       if (!cepInformado) {
         alert("Informe um CEP válido.");
         return;
       }
 
-      let urlCep = `https://brasilapi.com.br/api/cep/v2/${cepInformado}`;
+      if (localStorage.getItem(cepInformado)) {
+        let objCep = JSON.parse(localStorage.getItem(cepInformado));
+        console.log("Dados do CEP obtidos do localStorage:", objCep);
+        return this.getClima(objCep);
+      }
+
+      let cepFormatado = cepInformado.replace("-", "");
+      let urlCep = `https://brasilapi.com.br/api/cep/v2/${cepFormatado}`;
       let responseCep = await fetch(urlCep);
 
       if (!responseCep.ok) {
@@ -40,11 +54,24 @@ class OpenWeather {
       let objCep = await responseCep.json();
       console.log(objCep);
 
+      localStorage.setItem(cepInformado, JSON.stringify(objCep));
+
+      this.cep.push(cepInformado);
+      let listaCep = this.cep.join(";");
+      localStorage.cep = listaCep;
+
       return this.getClima(objCep);
     } catch (error) {
       console.error(error);
       alert("Erro ao buscar dados do CEP.");
+    } finally {
+      consultarBtn.disabled = false;
     }
+  }
+
+  recuperarCeps() {
+    let ceps = localStorage.cep ? localStorage.cep.split(";") : [];
+    return ceps;
   }
 
   async getClima(value) {
